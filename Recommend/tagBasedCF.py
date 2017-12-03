acc3015ef299481b3fce719f9b08668ade2af317
zhenghaishu <zhenghaishu@126.com>
2017-12-03 17:46:10 +0800

    .

--- Recommend/tagBasedCF.py
+++ Recommend/tagBasedCF.py
@@ -1,5 +1,6 @@
 #-*-coding:utf-8-*-
 import pdb
+import math
 
 user_items = dict()
 user_tags = dict()
@@ -21,7 +22,6 @@ def InitStat():
     line = data_file.readline()
     while line:
         terms = line.split("\t")
-        print(terms)
         user = terms[0]
         item = terms[1]
         tag = terms[2]
@@ -49,7 +49,6 @@ def Recommend(user):
 
 # 统计标签流行度
 def TagPopularity():
-    pdb.set_trace()
     tagFreq = {}
     for user in user_tags.keys():
         for tag in user_tags[user].keys():
@@ -59,32 +58,37 @@ def TagPopularity():
                 tagFreq[tag] += 1
     return sorted(tagFreq.items(), key = lambda a:a[1], reverse = True)
 
-#计算余弦相似度
+
+#计算余弦相似度
 def CosineSim(item_tags,i,j):
     ret = 0
-    for b,wib in item_tags[i].items():     #求物品i,j的标签交集数目
-        if b in item_tags[j]:
-            ret += wib * item_tags[j][b]
-    ni = 0
-    nj = 0
-    for b, w in item_tags[i].items():      #统计 i 的标签数目
-        ni += w * w
-    for b, w in item_tags[j].items():      #统计 j 的标签数目
-        nj += w * w
+    for tag,w in item_tags[i].items():     #求物品i,j的标签交集数目
+        if tag in item_tags[j]:
+            ret += w * item_tags[j][tag]
+            
     if ret == 0:
         return 0
+    
+    ni = 0
+    nj = 0
+    for tag, wi in item_tags[i].items():      #统计 i 的标签数目
+        ni += wi * wi
+    for tag, wj in item_tags[j].items():      #统计 j 的标签数目
+        nj += wj * wj
+
     return ret/math.sqrt(ni * nj)          #返回余弦值
 
 def Diversity(item_tags, recommend_items):
     ret = 0
     n = 0
-    for i in dict(recommend_items).keys():
-        for j in dict(recommend_item).keys():
+
+    for i in dict(recommend_items).keys():
+        for j in dict(recommend_items).keys():
             if i == j:
                 continue
             ret += CosineSim(item_tags, i ,j)
-            n += 1
-    return ret / (n * 1.0)
+            n += 1
+    return ret / n
     
 InitStat()
 
@@ -93,3 +97,6 @@ print(recommend_list)
 
 tagFreq = TagPopularity()
 print(tagFreq)
+
+diversityNum = Diversity(item_tags, recommend_list)
+print(diversityNum)
